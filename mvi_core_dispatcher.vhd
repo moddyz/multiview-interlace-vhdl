57a2676e045c8178564e4808ffd703ca18e0f172
Richard Lei <moddy3d@gmail.com>
2013-07-26 22:54:41 +0800

    Still reformatting the tabspaces into normal space

--- mvi_core_dispatcher.vhd
+++ mvi_core_dispatcher.vhd
@@ -13,9 +13,9 @@ entity mvi_core_dispatcher is
     Port ( fmt_width, fmt_height : in  UNSIGNED (11 downto 0);
            c0_x : out  UNSIGNED(11 downto 0);
            c0_y : out  UNSIGNED(11 downto 0);
-	   c0_clk : out STD_LOGIC;
-	   fin : out STD_LOGIC;
-	   clk : in  STD_LOGIC);
+       c0_clk : out STD_LOGIC;
+       fin : out STD_LOGIC;
+       clk : in  STD_LOGIC);
 end mvi_core_dispatcher;
 
 architecture Behavioral of mvi_core_dispatcher is
@@ -28,24 +28,24 @@ signal cnt_y : UNSIGNED (11 downto 0) := (others => '0');
 signal s_fin : STD_LOGIC := '0';
 
 begin
-	-- Core Processing Pixel Dispatching Process
-	dispatch_proc: process(clk)
-	begin
-		if rising_edge(clk) then
-			-- if x has not passed the frame width, increment x
-			if ((cnt_x + 1) < fmt_width) then
-				cnt_x <= cnt_x + 1;
-			else
-			-- if x passed the frame width, reset x, increment y
-				cnt_y <= cnt_y + 1;
-				cnt_x <= "000000000000";
-				-- if y have passed the frame height, send "finished' signal
-				if ((cnt_y + 1) = fmt_height) then
-					s_fin <= '1';
-				end if;
-			end if;
-		end if;
-	end process;
+    -- Core Processing Pixel Dispatching Process
+    dispatch_proc: process(clk)
+    begin
+        if rising_edge(clk) then
+            -- if x has not passed the frame width, increment x
+            if ((cnt_x + 1) < fmt_width) then
+                cnt_x <= cnt_x + 1;
+            else
+            -- if x passed the frame width, reset x, increment y
+                cnt_y <= cnt_y + 1;
+                cnt_x <= "000000000000";
+                -- if y have passed the frame height, send "finished' signal
+                if ((cnt_y + 1) = fmt_height) then
+                    s_fin <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
 
 fin  <= s_fin; -- Are all the pixels processed?
 c0_x <= cnt_x; -- Core 0 to process pixel (X, 
--- mvi_system.vhd
+++ mvi_system.vhd
@@ -10,92 +10,92 @@ use ieee_proposed.fixed_pkg.all;
 entity mvi_system is
     Port ( fmt_width    : IN  UNSIGNED(11 downto 0);
            fmt_height   : IN  UNSIGNED(11 downto 0);
-           angle 	: IN  SFIXED(31 downto 0);
-           views 	: IN  UNSIGNED(3 downto 0);
-           clk 		: IN  STD_LOGIC;
-           fin 		: OUT STD_LOGIC;
-	   c0_out_rgb   : OUT UNSIGNED(23 downto 0)); -- Testing purposes only, core RGB output
+           angle     : IN  SFIXED(31 downto 0);
+           views     : IN  UNSIGNED(3 downto 0);
+           clk         : IN  STD_LOGIC;
+           fin         : OUT STD_LOGIC;
+       c0_out_rgb   : OUT UNSIGNED(23 downto 0)); -- Testing purposes only, core RGB output
 end mvi_system;
 
 architecture Behavioral of mvi_system is
-	
-	-- System Internal Signals
-	signal i_fmt_width  	  : UNSIGNED(11 downto 0);
-   	signal i_fmt_height 	  : UNSIGNED(11 downto 0);
-	signal i_angle		  : SFIXED(31 downto 0);
-	signal i_views		  : UNSIGNED(3 downto 0);
-	signal i_clk		  : STD_LOGIC;
-	signal i_fin 		  : STD_LOGIC;
-	
-	-- Core Internal Signals
-	signal i_c0_clk	  	: STD_LOGIC;
-	signal i_c0_x 		: UNSIGNED(11 downto 0);
-	signal i_c0_y 		: UNSIGNED(11 downto 0);
-	signal i_c0_out_rgb 	: UNSIGNED(23 downto 0);
-	
+    
+    -- System Internal Signals
+    signal i_fmt_width        : UNSIGNED(11 downto 0);
+       signal i_fmt_height       : UNSIGNED(11 downto 0);
+    signal i_angle          : SFIXED(31 downto 0);
+    signal i_views          : UNSIGNED(3 downto 0);
+    signal i_clk          : STD_LOGIC;
+    signal i_fin           : STD_LOGIC;
+    
+    -- Core Internal Signals
+    signal i_c0_clk          : STD_LOGIC;
+    signal i_c0_x         : UNSIGNED(11 downto 0);
+    signal i_c0_y         : UNSIGNED(11 downto 0);
+    signal i_c0_out_rgb     : UNSIGNED(23 downto 0);
+    
 
-	-- Core Dispatcher Component
-	COMPONENT mvi_core_dispatcher
-	PORT(
-		fmt_width  	  : IN UNSIGNED(11 downto 0);
-		fmt_height 	  : IN UNSIGNED(11 downto 0);      
-		c0_x 		  : OUT UNSIGNED(11 downto 0);
-		c0_y 		  : OUT UNSIGNED(11 downto 0);
-		c0_clk	  	  : OUT STD_LOGIC;
-		clk 		  : IN STD_LOGIC;   
-		fin 		  : OUT STD_LOGIC
-		);
-	END COMPONENT;
-	
-	-- Core 
-	COMPONENT mvi_core
-	PORT(
-		fmt_width  	: IN  UNSIGNED(11 downto 0);
-      		fmt_height 	: IN  UNSIGNED(11 downto 0);
-		pos_x 	  	: IN UNSIGNED(11 downto 0);
-		pos_y 	  	: IN UNSIGNED(11 downto 0);
-		period_y   	: IN SFIXED(15 downto -16);
-		period_x   	: IN UNSIGNED(3 downto 0);          
-		out_rgb 	: OUT UNSIGNED(23 downto 0);
-		clk		: IN STD_LOGIC
-		);
-	END COMPONENT;
+    -- Core Dispatcher Component
+    COMPONENT mvi_core_dispatcher
+    PORT(
+        fmt_width        : IN UNSIGNED(11 downto 0);
+        fmt_height       : IN UNSIGNED(11 downto 0);      
+        c0_x           : OUT UNSIGNED(11 downto 0);
+        c0_y           : OUT UNSIGNED(11 downto 0);
+        c0_clk            : OUT STD_LOGIC;
+        clk           : IN STD_LOGIC;   
+        fin           : OUT STD_LOGIC
+        );
+    END COMPONENT;
+    
+    -- Core 
+    COMPONENT mvi_core
+    PORT(
+        fmt_width      : IN  UNSIGNED(11 downto 0);
+              fmt_height     : IN  UNSIGNED(11 downto 0);
+        pos_x           : IN UNSIGNED(11 downto 0);
+        pos_y           : IN UNSIGNED(11 downto 0);
+        period_y       : IN SFIXED(15 downto -16);
+        period_x       : IN UNSIGNED(3 downto 0);          
+        out_rgb     : OUT UNSIGNED(23 downto 0);
+        clk        : IN STD_LOGIC
+        );
+    END COMPONENT;
 
 begin
-	
-	-- Input signal assignment
-	i_fmt_width <= fmt_width;
-	i_fmt_height <= fmt_height;
-	i_angle <= angle;
-	i_views <= views;
-	i_clk <= clk;
-	
-	-- Output signal assignment
-	fin <= i_fin;
-	c0_out_rgb <= i_c0_out_rgb;
-	
-	-- Dispatcher Instantiation
-	CORE_DISPATCHER: mvi_core_dispatcher PORT MAP(
-		fmt_width => i_fmt_width,
-		fmt_height => i_fmt_height,
-		clk => i_clk,
-		c0_x => i_c0_x,
-		c0_y => i_c0_y,
-		c0_clk => i_c0_clk,
-		fin => i_fin
-	);
-	
-	-- Insantiation of Core(s)
-	CORE_N0: mvi_core PORT MAP(
-		fmt_width => i_fmt_width,
-		fmt_height => i_fmt_height,
-		pos_x => i_c0_x,
-		pos_y => i_c0_y,
-		period_y => i_angle, -- Need to add an angle to period.y converter
-		period_x => i_views,
-		out_rgb =>  i_c0_out_rgb,
-		clk => i_c0_clk
-	);
-	
+    
+    -- Input signal assignment
+    i_fmt_width <= fmt_width;
+    i_fmt_height <= fmt_height;
+    i_angle <= angle;
+    i_views <= views;
+    i_clk <= clk;
+    
+    -- Output signal assignment
+    fin <= i_fin;
+    c0_out_rgb <= i_c0_out_rgb;
+    
+    -- Dispatcher Instantiation
+    CORE_DISPATCHER: mvi_core_dispatcher PORT MAP(
+        fmt_width => i_fmt_width,
+        fmt_height => i_fmt_height,
+        clk => i_clk,
+        c0_x => i_c0_x,
+        c0_y => i_c0_y,
+        c0_clk => i_c0_clk,
+        fin => i_fin
+    );
+    
+    -- Insantiation of Core(s)
+    CORE_N0: mvi_core PORT MAP(
+        fmt_width => i_fmt_width,
+        fmt_height => i_fmt_height,
+        pos_x => i_c0_x,
+        pos_y => i_c0_y,
+        period_y => i_angle, -- Need to add an angle to period.y converter
+        period_x => i_views,
+        out_rgb =>  i_c0_out_rgb,
+        clk => i_c0_clk
+    );
+    
 end Behavioral;
 
